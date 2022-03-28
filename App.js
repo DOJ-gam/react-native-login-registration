8bb03761aa1d022cb249ccf825d7d92e54743e4f
doj-gam <omarjeng999@gmail.com>
2022-03-28 00:23:08 +0000

    add auth

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
@@ -5,18 +6,53 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Loader from "./components/Loader";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [initialRouteName, setInitialRouteName] = React.useState("");
+
+  React.useEffect(() => {
+    setTimeout(authUser, 2000);
+  }, []);
+
+  const authUser = async () => {
+    try {
+      let userData = await AsyncStorage.getItem("user");
+      if (userData) {
+        //if data means user has registered
+        userData = JSON.parse(userData);
+        if (userData?.loggedIn) {
+          setInitialRouteName("Home");
+        } else {
+          setInitialRouteName("Login");
+        }
+      } else {
+        setInitialRouteName("Register");
+      }
+    } catch (error) {
+      setInitialRouteName("Register");
+    }
+  };
+
   return (
     <NavigationContainer>
-      <StatusBar style="transparent" />
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      {initialRouteName == "" ? (
+        <Loader visible={true} />
+      ) : (
+        <>
+          <StatusBar style="transparent" />
+          <Stack.Navigator
+            initialRouteName={initialRouteName}
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </>
+      )}
     </NavigationContainer>
   );
 }
