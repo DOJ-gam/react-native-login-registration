45ef176cf1817d2d0df5cddf738d9875029bbe8c
doj-gam <omarjeng999@gmail.com>
2022-03-28 00:06:51 +0000

    loggout functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { DformContainer, Dview } from "react-native-ui-doj";
+import { StyleSheet } from "react-native";
+import { Dtext, Dview } from "react-native-ui-doj";
 import HomeHeader from "../components/HomeHeader";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Button from "../components/Button";
 const Home = ({ navigation }) => {
+  const [userDetails, setUserDetails] = React.useState();
+
+  React.useEffect(() => {
+    getUserDetails();
+  }, []);
+
+  const logout = () => {
+    AsyncStorage.setItem(
+      "user",
+      JSON.stringify({ ...userDetails, loggedIn: false })
+    );
+    navigation.navigate("Login");
+  };
+
+  const getUserDetails = async () => {
+    const userData = await AsyncStorage.getItem("user");
+    if (userData) {
+      setUserDetails(JSON.parse(userData));
+    }
+  };
   return (
     <Dview style={styles.container}>
       <HomeHeader />
+      <Dtext fs={40} mt={20} text="center" fw="bold">
+        Welcome {userDetails?.fullname}
+      </Dtext>
+      <Button title="logout" onPress={logout} />
     </Dview>
   );
 };
